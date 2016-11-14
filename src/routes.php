691c8b5f1182bd21e65aa2f58309fe1a9a04a423
acorolus <a.corolus@gmail.com>
2016-11-15 00:21:04 +0100

    new file:   public/index.css 	modified:   src/routes.php 	new file:   templates/ajout.html 	new file:   templates/boutique.html 	new file:   templates/index.css 	modified:   templates/index.phtml

--- src/routes.php
+++ src/routes.php
@@ -1,10 +1,25 @@
 <?php
 // Routes
 
-$app->get('/[{name}]', function ($request, $response, $args) {
+$app->get('/boutique', function ($request, $response, $args) {
+    // Sample log message
+    $this->logger->info("Slim-Skeleton '/' route");
+
+    // Render index view
+    return $response->renderer->render($response, 'boutique.html', $args);
+});
+
+
+
+
+$app->get('/', function ($request, $response, $args) {
     // Sample log message
     $this->logger->info("Slim-Skeleton '/' route");
 
     // Render index view
     return $this->renderer->render($response, 'index.phtml', $args);
 });
+
+
+
+
